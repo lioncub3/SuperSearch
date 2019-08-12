a4ce4dd8003b6565afed14ef07cad9f5044c53fb
Olexmo <ol.shyba@gmail.com>
2019-08-12 20:20:23 +0300

    init

---
+++ SuperSearch/Models/SearchObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperSearch.Models
+{
+    public class SearchObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
